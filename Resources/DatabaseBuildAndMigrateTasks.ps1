3e53b930e206778a6fb9528e8b8b0498d116a772
Phil Factor <philipFactor@gmail.com>
2023-04-05 12:24:25 +0100

    Update DatabaseBuildAndMigrateTasks.ps1

--- Resources/DatabaseBuildAndMigrateTasks.ps1
+++ Resources/DatabaseBuildAndMigrateTasks.ps1
@@ -101,6 +101,13 @@ This connects to the SQL Server database and will then, For the current version 
  - *'Schema'* (files in folders for each schema),
  - *'ObjectType'*  (files in folders for each object type), 'Flat' (all files in the same folder)
  - *'File'* (1 single file). 
+
+**$CreatePossibleMigrationScriptFromDacpac**  *(SQL Server only)*
+   By default, this will take the DACPAC for the current version of the database, and will 
+   compare it with the previous version. it should, logically, work either way to do forward 
+   or UNDO migrations. Sadly it doesn't always work. The file is processed to remove  any CREATE SCHEMA
+   statements,SQLCMD statements and any SQLCMD parameter definitions
+
 ## examples of usage
 
 Tasks can be executed one at a time or stacked up and executed one after another. 
@@ -4249,6 +4256,65 @@ $ExtractFromSQLServerIfNecessary = { <#
 	
 }
 
+<# By default, this will take the DACPAC for the current version of the database, and will 
+   compare it with the previous version. it should, logically, work either way to do forward 
+   or UNDO migrations. Sadly it doesn't. The file is processed to remove  any CREATE SCHEMA
+   statements,SQLCMD statements and any SQLCMD parameter definitions #>
+$CreatePossibleMigrationScriptFromDacpac = { 
+	Param ($param1, # the dbDetails array where we pass all the info
+		$OutputScriptType = 'Versioned',
+        [bool]$OverWrite = $true
+	) # $CreatePossibleMigrationScriptFromDacpac  (Don't delete this) 
+	$problems = @(); # well, not yet
+	$feedback = @()
+	@('version', 'previous') |
+		foreach{
+			if ($param1.$_ -in @($null, ''))
+			{
+				Process-FlywayTasks $param1 $GetCurrentVersion;
+			}
+		}
+		
+	@('version', 'previous', 'project', 'reportLocation') | foreach{
+		if ($param1.$_ -in @($null, '')) { $WeCanDoIt = $false; $Problems += "no value for '$($_)'" }
+	}
+	$EscapedProject = ($Param1.project.Split([IO.Path]::GetInvalidFileNameChars()) -join '_') -ireplace '\.', '-'
+	$ReportDirectory = "$($param1.reportLocation)\Versions";
+	#default to undo
+	$SourceVersion = $param1.previous
+	$TargetVersion = $param1.version
+	$prefix = 'U';
+    if ($OutputScriptType -eq 'versioned')
+	{
+		$SourceVersion = $param1.version
+		$TargetVersion = $param1.previous
+        $prefix = 'V'
+	}
+	if ([version]$SourceVersion -gt [version]$TargetVersion) {$prefix = 'U'};
+	$SourceFile = "$ReportDirectory\$SourceVersion\$EscapedProject$SourceVersion.DACPAC";
+	$TargetFile = "$ReportDirectory\$TargetVersion\$EscapedProject$TargetVersion.DACPAC";
+	if ($prefix -eq 'U')
+	{ $OutputFile = "$ReportDirectory\$TargetVersion\$($prefix)_$($TargetVersion)__UndoMigration_to_$($SourceVersion).sql" }
+	else
+	{ $OutputFile = "$ReportDirectory\$SourceVersion\$($prefix)_$($SourceVersion)__ForwardMigration_from_$($TargetVersion).sql" };
+	@($SourceFile, $TargetFile) | foreach {
+		if (-not (Test-Path "$_")) { $WeCanDoIt = $false; $Feedback += "cannot find the dacpac file $_ " }
+	}
+	if ($OverWrite -eq $false -and (Test-Path "$OutputFile"))
+         { $WeCanDoIt = $false; $Feedback += "Migration file $OutputFile already exists" }
+	if ($WeCanDoIt) #if it has passed the tests
+	{
+    $Result=Export-ChangeScriptFromDACPACS `
+         -SourceDacPac $sourceFile -TargetDacPac $TargetFile -OutputFilePath $OutputFile -OverWrite $overwrite -DatabaseName $param1.project
+    if ($Result[0].count -gt 0)
+	{ $Param1.problems.'CreatePossibleMigrationScriptFromDacpac' = $Result[1] }
+    if ($Result[1].count -gt 0)
+	{ $Param1.feedback.'CreatePossibleMigrationScriptFromDacpac' = $Result[1] }
+	if ($Result[2].count -gt 0)
+	{$Param1.WriteLocations.'CreatePossibleMigrationScriptFromDacpac' = $Result[2];
+		}
+	}
+}
 
 <# this creates a first-cut UNDO script for the metadata (not the data) which can
 be adjusted and modified quickly to produce an UNDO Script. It does this by using
@@ -5496,9 +5562,150 @@ function Run-TestsForMigration
 	}
 }
 
+<#
+	.SYNOPSIS
+		Compare two DACPACs and create a change script from them using SQLPackage that
+		Creates a Transact-SQL incremental update script that updates the schema of a 
+        target DACPAC to match the schema of a source DACPAC.
+	
+	.DESCRIPTION
+		This compares two dacpacs and creates a schange script from them It is a thin shell over SQLPackage.
+	
+	.PARAMETER SourceDacPac
+		The path to the source Dacpac
+	
+	.PARAMETER TargetDacPac
+		The path to the The target DACPAC
+	
+	.PARAMETER OutputFilePath
+		The path to the change script
+	
+	.PARAMETER OverWrite
+		Do we overwrite the generated script if it already exists?
+	
+	.EXAMPLE
+		PS C:\> Export-ChangeScriptFromDACPACS -SourceDacPac $path1 -TargetDacPac $path2 -OutputFilePath $path3
+	
+	.NOTES
+		This is designed to support various specific script-generation tasks
+#>
+function Export-ChangeScriptFromDACPACS
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		$SourceDacPac,
+		[Parameter(Mandatory = $true)]
+		$TargetDacPac,
+		[Parameter(Mandatory = $true)]
+		$OutputFilePath,
+		[bool]$OverWrite = $false,
+        $DatabaseName ='Database'
+	)
+	
+<# The file is processed to remove  any CREATE SCHEMA statements, and any SQLCMD parameter
+ definitions #>
+	$problems = @(); # well, not yet
+	$feedback = @()
+	$WeCanDoIt = $true # until proven otherwise
+	# the alias must be set to the path of your installed version of SQLpackage
+	$command = get-command sqlpackage -ErrorAction Ignore
+	if ($command -eq $null)
+	{
+		if ($SQLPackageAlias -ne $null)
+		{ Set-Alias sqlpackage   $SQLPackageAlias }
+		else
+		{ $problems += 'You must have provided a path to $SQLPackage.exe in the ToolLocations.ps1 file in the resources folder' }
+	}
+	
+	@($SourceDacPac, $TargetDacPac) | foreach {
+		if (-not (Test-Path "$_")) { $WeCanDoIt = $false; $Feedback += "cannot find the dacpac file $_ " }
+	}
+	if ($OverWrite -eq $false -and (Test-Path "$OutputFilePath")) { $WeCanDoIt = $false; $Feedback += "Migration file $OutputFilePath already exists" }
+	if ($WeCanDoIt) #if it has passed the tests
+	{
+		$ScriptArguments = @(
+    <# Creates a Transact-SQL incremental update script that updates the schema of a 
+       target to match the schema of a source. #>
+			"/Action:Script", <#
+         Specifies a source file to be used as the source of action instead of a
+         database. For the Publish and Script actions, SourceFile may be a .dacpac
+         file or a schema compare .scmp file. If this parameter is used, no other
+         source parameter is valid. #>
+			"/SourceFile:$SourceDACPAC",
+         <#Specifies a source file to be used as the source of action instead of a
+         database. For the Publish and Script actions, SourceFile may be a .dacpac
+         file or a schema compare .scmp file. If this parameter is used, no other
+         source parameter is valid. #>
+			"/OutputPath:$OutputFilePath", <#
+         Specifies the file path where the output files are generated. (short form
+         /op)#>
+			"/OverwriteFiles:$OverWrite", <#
+         Specifies if SqlPackage.exe should overwrite existing files. Specifying
+         false causes SqlPackage.exe to abort action if an existing file is
+         encountered. Default value is True. (short form /of)"/TargetUser:<string>",<#
+         For SQL Server Auth scenarios, defines the SQL Server user to use to
+         access the target database. (short form /tu) #>
+			"/TargetDatabaseName:$DatabaseName",
+			"/TargetFile:$TargetDacPac",    <# Specifies a target file (that is, a 
+			.dacpac file) to be used as the target of action instead of a database.
+			If this parameter is used, no other target parameter shall be valid. 
+			This parameter shall be invalid for actions that only support database
+			targets.#>
+			'/p:CommentOutSetVarDeclarations=true'
+			"/p:CreateNewDatabase=False"
+		)
+		$console = sqlpackage $ScriptArguments
+		$Feedback += "$console"
+		if ($?)
+		{
+            $TargetVersion=split-path -Path $TargetDacPac -Leaf
+			$SourceVersion=split-path -Path $SourceDacPac -Leaf
+            $feedback += "Written $prefix migration for $($param1.Project) from $TargetVersion to $SourceVersion" 
+		}
+		else # if no errors then simple message, otherwise....
+		{
+			#report a problem and send back the args for diagnosis (hint, only for script development)
+			$Arguments = '';
+			$Arguments += $ScriptArguments | foreach{ $_ }
+			$Problems += "Script generation Went badly. (code $LASTEXITCODE) with paramaters $Arguments"
+		}
+		if ($problems.count -eq 0)
+		{
+			# now convert all the SQLcmd output and other things not allowed
+			$script = [IO.File]::ReadAllText($OutputFilePath)
+			
+			@(
+				@(@'
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'SQLCMD mode must be enabled to successfully execute this script.';
+        SET NOEXEC ON;
+    END
+'@, ''),
+				@(@'
+GO
+USE [$(DatabaseName)];
+'@, '')
+			) | foreach {
+				$Script = $script.replace($_[0], $_[1])
+			}
+			# remove code to create schemas This has to be done by a regex
+			$Script = $Script -creplace ':setvar.*|:on error exit', ''
+			$Script = $Script -creplace '(\n|\r)+\s(\n|\r)+', "`n"
+			$Script = $Script -creplace '(?s)PRINT N''Creating Schema \[.{1,256}?\]...'';\s+?GO\s+?CREATE SCHEMA \[.{1,256}?\].+?GO', ''
+			# and write the script back
+			[System.IO.File]::WriteAllLines($OutputFilePath, $script);
+			$WriteLocations = "$OutputFilePath";
+		}
+	}	
+    @($problems, $Feedback, $WriteLocations)
+}
 
 
-
-'FlywayTeamwork framework  loaded. V1.2.606'
+'FlywayTeamwork framework  loaded. V1.2.610'
 
 
