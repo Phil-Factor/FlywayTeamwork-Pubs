aca657856066b72efa4a69a9a5b4177b95201bc0
Phil Factor <philipFactor@gmail.com>
2023-10-25 17:01:37 +0100

    tweaks to Do-AFlywayCommand

---
+++ PubsPG/Branches/Develop/scripts/afterMigrate__SendMessage.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Before using this, write two placeholders in your flyway.conf file. It can easily go
+in the flyway.conf file in your user area.
+
+   flyway.placeholders.Message=no message
+   flyway.placeholders.TheWebhookUrl=https://<Whatever_webhook_you've_been_Assigned>
+
+
+Send a message that is placed in the placeholder 'Message'. This will normally be 
+placed in the FP__Message__ environment variable.
+
+You specify a message at the command-line like this
+
+   flyway migrate "-placeholders.Message=Ran a Pubs migration for Oracle" 
+if you want to expressly prevent a message set it to 'No message'
+
+#>
+$TheMessage = "$($env:FP__Message__)"
+$TheURI = "$($env:FP__TheWebhookUrl__)"
+$MessageExists = !([string]::IsNullOrWhiteSpace($TheMessage));
+$URIExists = !([string]::IsNullOrWhiteSpace($TheURI));
+if ($TheMessage -ne 'no message')
+{
+	$messageObject = @{
+		text =  @"
+From: $($ENV:FP__flyway_user__)
+on: $($ENV:FP__flyway_timestamp__)
+After migration on $($ENV:FP__flyway_database__)
+$($env:FP__Message__) 
+"@
+	}
+	# Convert the payload object to JSON
+	$messageJson = $messageObject | ConvertTo-Json
+	$postParams = @{ payload = $messagejson }
+	Invoke-RestMethod -Uri "$($env:FP__TheWebhookUrl__)" -Method POST -ContentType "application/json" -Body $postParams
+	$env:FP__Message__ = 'No message'
+}
+if (!$MessageExists) { Write-warning "No default message Exists" }
+if (!$URIExists) { Write-warning "No $URIExists Exists" }
--- Resources/Do_AFlywayComand.ps1
+++ Resources/Do_AFlywayComand.ps1
@@ -1,70 +1,115 @@
 ﻿<#
 	.SYNOPSIS
 		Executes a Flyway command using Flyway CLI, with the parameters you choose
-        and with an optional path to a secrets file. It sends warnings to the warning
-        channel, chatter to the -verbose channel and it sends errors either to the
-        screen or to a function (e.g. webhook or logging) that you specify. 
+		and with an optional path to a secrets file. It sends warnings to the warning
+		channel, chatter to the -verbose channel and it sends errors either to the
+		screen or to a function (e.g. webhook or logging) that you specify.
 	
 	.DESCRIPTION
 		This function is used where you need to differentiate the noise (verbose)
-        from the warnings and errors for long migrations. 
+		from the warnings and errors for long migrations.
 		It is also handy if you need to store configurations in secure directories,
-        when you get unexpectedly asked for passwords.
-        It is designed to allow you to save the error message, add them to a log or
-        send them to a notification system 
+		when you get unexpectedly asked for passwords.
+		It is designed to allow you to save the error message, add them to a log or
+		send them to a notification system
 	
 	.PARAMETER Parameters
 		An options array of parameters that specify how Flyway should do the migration.
 	
 	.PARAMETER Secrets
 		an optional path to a 'secrets' .conf file. Note that this routine does not
-        allow Flyway to interrogate the user to provide a password. If it does so,
-        it is provided with a dummy password
-
-    .PARAMETER ErrorNotifier
+		allow Flyway to interrogate the user to provide a password. If it does so,
+		it is provided with a dummy password
+	
+	.PARAMETER ErrorNotifier
 		an optional powershell scriptblock that alerts your notification system of
-         an error 
+		an error
+	
+	.PARAMETER name
+		The name of the action being done, for reporting purposes
 	
 	.EXAMPLE
 		$Result= Do-AFlywayCommand @("migrate", "-target=1.1.4") $Secrets -Verbose
-
-    .EXAMPLE
+	
+	.EXAMPLE
+		get info on current project in the working directory, taking the connection info and
+		credentials from the flyway.conf
+		'info'| Do-AFlywayCommand -verbose
+	
+	.EXAMPLE
+		get info on current project in the working directory, taking the connection info and
+		credentials from the specified file in the user area
+		$Secrets='MySecrets.conf'
 		
-
-    .EXAMPLE
-        Do-AFlywayCommand   @("-url=AnUnusableURL") '' -ErrorNotifier $OurErrorNotifier
-
-    .Example 
-        Do-AFlywayCommand -ErrorNotifier { param($TheErrorMessage)
-            $FlywayChannel='https://OurWebhook/webapi/entry.cgi?api=MyApp'
-            Send-SynologyChatMessage $FlywayChannel "In $pwd $TheErrorMessage"
-        }
-
+		'info'| Do-AFlywayCommand -Secrets $Secrets
+	
+	.EXAMPLE
+		get info followed by a migrate  on current project in the working directory,
+		taking the connection info and credentials from the specified file in the user
+		area
+		
+		'info','migrate'|foreach {
+		Do-AFlywayCommand -parameters $_ -Secrets $Secrets -name 'MySQL Pubs Main branch' -Verbose
+		}
+	
+	.EXAMPLE
+		get info followed by running a clean, and finally doing a migrate  on two differet
+		projects; taking the connection info and credentials from the specified file in the user
+		area
+		@(
+		@{'Directory'='<MyPathTo>\pubs';
+		'secrets'='Pubs_SQLServer_Main_<MyServer>.conf';
+		'Name'='Pubs on the router'}
+		@{'Directory'='<MyPathTo>\pubs\Branches\develop';
+		'secrets'='Pubs_SQLServer_Develop_<MyServer>.conf';
+		'Name'='Pubs Dev branch on the router'}
+		)| foreach {
+		cd $_.Directory;
+		$Secrets=$_.Secrets
+		$name=$_.Name
+		@('info','clean','migrate')| foreach{
+		Do-AFlywayCommand -parameters $_ -Secrets $Secrets -Name $Name -Verbose
+		}
+		}
+	
+	.EXAMPLE
+		Do-AFlywayCommand   @("-url=AnUnusableURL") '' -ErrorNotifier $OurErrorNotifier
+	
+	.Example
+		Do-AFlywayCommand -ErrorNotifier { param($TheErrorMessage)
+		$FlywayChannel='https://OurWebhook/webapi/entry.cgi?api=MyApp'
+		Send-SynologyChatMessage $FlywayChannel "In $pwd $TheErrorMessage"
+		}
+	
+	.NOTES
+		Additional information about the function.
 #>
 function Do-AFlywayCommand
 {
 	[CmdletBinding()]
 	param
 	(
-		[Parameter(Mandatory = $False,
+		[Parameter(Mandatory = $false,
 				   ValueFromPipeline = $true,
 				   HelpMessage = 'The parameters that specify what you wish  Flyway to do ')]
 		[array]$parameters = @(),
-		[Parameter(Mandatory = $false)]
+		[Parameter(Mandatory = $false,
+				   HelpMessage = 'The path to an extra .conf file. it looks in the user area is you just provide a filename')]
 		[String]$Secrets = '',
 		[Parameter(Mandatory = $false,
-				   HelpMessage = 'The scriptblock for hamdling errors ')]
+				   HelpMessage = 'The scriptblock for handling or reporting errors ')]
 		[scriptblock]$ErrorNotifier = { },
-        [Parameter(Mandatory = $false,
-                   HelpMessage = 'The name of the action ') ]
-		[String]$Name= 'current database'
+		[Parameter(Mandatory = $false,
+				   HelpMessage = 'The name of the description of the  flyway action  being done')]
+		[String]$Name = 'current database'
 	)
-	write-verbose "now doing flyway command with $Name"
+	
+	write-verbose "now doing flyway command $Parameters with $Name"
 	$ExtraParameters = { $parameters }.Invoke() # get any extra parameters
 	if (!([string]::IsNullOrEmpty($Secrets))) # if you've specified an extra .conf file 
 	{
-       if (-not (Test-Path $Secrets))
-            { $Secrets="$($env:USERPROFILE)\$Secrets" }
+		if (-not (Test-Path $Secrets))
+		{ $Secrets = "$($env:USERPROFILE)\$Secrets" }
 		get-content $Secrets | foreach {
 			$_.Split("`n") |
 			where { ($_ -notlike '#*') -and ("$($_)".Trim() -notlike '') } |
